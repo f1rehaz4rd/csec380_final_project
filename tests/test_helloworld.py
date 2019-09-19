f8c67ede544ddcbd5562cb563ff8fc73bc48cb79
JonBauer123 <wcjon123@gmail.com>
2019-09-18 23:13:08 -0400

    THIS WILL WORK

--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -4,4 +4,5 @@ import requests
 def getContent(url):
     return str(requests.get(url).content)
 
-assert "Hello World" in getContent("http://localhost")
+def test_server():
+    assert "Hello World" in getContent("http://localhost")
