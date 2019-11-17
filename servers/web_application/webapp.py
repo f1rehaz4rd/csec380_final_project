c70c58a3b1d8bdc091b8f96b22d405bbedf95207
JonBauer123 <wcjon123@gmail.com>
2019-11-16 22:02:21 -0500

    Fixed uploading with video url. Still a few issues

--- servers/web_application/webapp.py
+++ servers/web_application/webapp.py
@@ -20,6 +20,7 @@ from werkzeug.utils import secure_filename
 
 import secrets
 import os
+import requests as getVideo
 
 ### Setting up the Flask App, Session Manager, and Database
 app = Flask(__name__, template_folder="templates")
@@ -119,6 +120,12 @@ def uploaded_file(filename):
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_video():
+    """
+    Handles uplaoding a video.
+    TODO:
+        Add file already exists checking
+    """
+
     if not authorize():
         return redirect(url_for('login'))
 
@@ -127,27 +134,56 @@ def upload_video():
             return redirect(request.url)
 
         file = request.files['file']
-
-        if file.filename == '' or request.form['title'] == '':
-            return redirect(request.url)
-
+        
         # Need to add extension verificaiton 
-        if file and allowed_file(file.filename):
-            # Save the file to the server
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # Save the metadata to the database
-            url= "http://localhost:80/display/" + filename
+        if file and allowed_file(file.filename) and \
+                file.filename != '' and request.form['title'] != '':
+            
+            try:
+                # Save the file to the server
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # Save the metadata to the database
+                url= "http://localhost:80/display/" + filename
 
-            # NEED TO SANITIZE THE TITLE INPUT
+                # NEED TO SANITIZE THE TITLE INPUT
 
-            video = videos(title=request.form['title'],filename=filename, filetype=filename.split('.')[1],\
-                path=app.config['UPLOAD_FOLDER'], url=url, username=session.get('username'))
-            db.session.add(video)
-            db.session.commit()
+                video = videos(title=request.form['title'],filename=filename, filetype=filename.split('.')[1],\
+                    path=app.config['UPLOAD_FOLDER'], url=url, username=session.get('username'))
+                db.session.add(video)
+                db.session.commit()
 
-            return redirect(url_for('home'))
+                return redirect(url_for('home'))
+            except:
+                return redirect(request.url)
 
+
+        elif request.form['videourl'] is not None: # If it is a link that was submited and not a video
+            upload_url = request.form['videourl']
+
+            try:
+                req = getVideo.get(upload_url, allow_redirects=True)
+
+                filename = upload_url.split('/')
+                filename = filename[len(filename) - 1]
+                filename = secure_filename(filename) # Secures the filename
+                # Saves the file to the server
+                fd = open(app.config['UPLOAD_FOLDER'] + filename, "wb")
+                fd.write(req.content)
+                fd.close()
+
+                url= "http://localhost:80/display/" + filename
+
+                video = videos(title=request.form['title'],filename=filename, filetype=filename.split('.')[1],\
+                    path=app.config['UPLOAD_FOLDER'], url=url, username=session.get('username'))
+                db.session.add(video)
+                db.session.commit()
+
+                return redirect(url_for('home'))
+            except:
+                print("Oh no REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+                return redirect(url_for('testToken'))
+        
     return render_template("upload.html")
 
 @app.route('/token')
